aa8a1e3fb47982e4a200607b2293fece044c8d9d
adeahmed <abdi-samad.ahmed@outlook.com>
2024-06-05 21:21:33 +0300

    2

--- Main.py
+++ Main.py
@@ -6,10 +6,12 @@ import torch.optim as optim
 import numpy as np
 import os
 
-torch.set_default_tensor_type("torch.cuda.FloatTensor")
 
-SCREEN_WIDTH = 250
-SCREEN_HEIGHT = 250#250
+
+SCREEN_WIDTH = 150
+SCREEN_HEIGHT = 150#250
+
+
 
 FRUIT_SIZE = 10
 SNAKE_SIZE = 10
@@ -32,9 +34,9 @@ class Agent(nn.Module):
         self.fc2 = nn.Linear(1024, 512)
         self.fc4 = nn.Linear(512, 4)
         self.gamma = 0.99
-        self.epsilon = 0.007
+        self.epsilon = 0.000
         self.epsilon_decay = 0.999975
-        self.epsilon_min = 0.007
+        self.epsilon_min = 0.000
         self.lr = 0.0002
         self.memory = []
         self.optimizer = optim.Adam(self.parameters(), lr=self.lr)
@@ -130,6 +132,8 @@ class Snake:
         else:
             self.loc.pop()
         self.direction = direction
+        if self.loc[0] in self._game.visited:
+            return -1.1
         return -1
 
     def eat(self):
@@ -149,10 +153,13 @@ class Game:
         self.score = 0
         self.current_score = 0
         self.highest = self.score
+        self.points=0
+        self.font = pygame.font.Font('freesansbold.ttf', 32)
+        self.visited=[]
 
         # Load the model if exists
         try:
-            checkpoint = torch.load(self.model_path)
+            checkpoint = torch.load(self.model_path, map_location=torch.device('cpu'))
             self.agent.load_state_dict(checkpoint['model_state_dict'])
             self.agent.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
             print("Model loaded successfully!")
@@ -187,6 +194,9 @@ class Game:
                 # If the position is the future head, mark it as the future head
                 elif [x, y] == future_head:
                     grid[i + VIEW_SIZE//2][j + VIEW_SIZE//2] = 3  # 3 represents the future head
+                elif [x,y] in self.visited:
+                    for x,y in self.visited:
+                        grid[i+VIEW_SIZE//2][j+VIEW_SIZE//2]=-2 #old visited
                 else:
                     grid[i + VIEW_SIZE//2][j + VIEW_SIZE//2] = 0  # 0 represents an empty space
 
@@ -195,13 +205,17 @@ class Game:
         snake_prev_location_encoding = np.array([self.snake.prev_loc[0]//SNAKE_SIZE, self.snake.prev_loc[1]//SNAKE_SIZE])
         fruit_location_encoding = np.array([self.fruit.loc[0]//FRUIT_SIZE, self.fruit.loc[1]//FRUIT_SIZE])
         snake_length_encoding = np.array([len(self.snake.loc)])
+        print(np.concatenate((grid.flatten(), view_size_encoding, snake_location_encoding, fruit_location_encoding, snake_length_encoding, snake_prev_location_encoding)))
         return np.concatenate((grid.flatten(), view_size_encoding, snake_location_encoding, fruit_location_encoding, snake_length_encoding, snake_prev_location_encoding))
 
     def take_action(self, action):
         directions = ["UP", "DOWN", "LEFT", "RIGHT"]
         direction = directions[action]
         reward = self.snake.move(direction)
+        self.visited.append(self.snake.loc[0])
         if self.snake.loc[0] == list(self.fruit.loc):
+            self.visited=[]
+            self.points+=100
             self.snake.eat()
             self.fruit = Fruit()
             self.score += 1
@@ -212,6 +226,7 @@ class Game:
     def main_loop(self):
         while True:  # Run forever
             while self.running:
+                direction=self.snake.direction
                 if game.is_ai:
                     state = self.get_game_state()
                     action = self.agent.select_action(state)
@@ -235,8 +250,12 @@ class Game:
                                 direction = "LEFT"
                             elif event.key == K_RIGHT:
                                 direction = "RIGHT"
-                            self.snake.move(direction)
-
+                if not self.is_ai:
+                    if self.snake.loc[0] == list(self.fruit.loc):
+                        self.points+=100
+                        self.snake.eat()
+                        self.fruit = Fruit()
+                    self.snake.move(direction)
                 self.screen.fill((0, 0, 0))
 
                 for loc in self.snake.loc:
@@ -246,12 +265,29 @@ class Game:
                 fruit_block = pygame.Rect(*self.fruit.loc, FRUIT_SIZE, FRUIT_SIZE)
                 pygame.draw.rect(self.screen, self.fruit.color, fruit_block)
 
+                text = self.font.render(f'Yolo: {self.points}', True, (0,212,0))
+                #self.screen.blit(text,(SCREEN_WIDTH-text.get_width(),10))
+                head_x, head_y = self.snake.loc[0]
+                for i in range(-5, 5,1):
+                    for j in range(-5,5,1):
+                        new_x = head_x + i*SNAKE_SIZE
+                        new_y = head_y + j*SNAKE_SIZE
+                        if new_x < 0 or new_x >= SCREEN_WIDTH or new_y < 0 or new_y >= SCREEN_HEIGHT:
+                            continue
+                        if [new_x, new_y] in self.snake.loc:
+                            pygame.draw.rect(self.screen, (0, 150, 0), pygame.Rect(new_x, new_y, SNAKE_SIZE, SNAKE_SIZE))
+                        elif (new_x, new_y) == self.fruit.loc:
+                            pygame.draw.rect(self.screen, (150, 0, 0), pygame.Rect(new_x, new_y, FRUIT_SIZE, FRUIT_SIZE))
+                        else: 
+                            pygame.draw.rect(self.screen, (128, 128, 128), pygame.Rect(new_x, new_y, SNAKE_SIZE, SNAKE_SIZE))
                 pygame.display.flip()
                 if not self.is_ai:
                     self.clock.tick(10)
+                self.clock.tick(100)
 
             # Save the model and optimizer upon death
-            torch.save({
+            if self.is_ai:
+                torch.save({
                 'model_state_dict': self.agent.state_dict(),
                 'optimizer_state_dict': self.agent.optimizer.state_dict(),
             }, self.model_path)
@@ -260,7 +296,7 @@ class Game:
             self.game_counter += 1
             self.highest = max(self.current_score, self.highest)
             self.current_score = 0
-            os.system("cls")
+            os.system("clear")
             print(f"Highest score of current agent during this run is: {self.highest}")
             print(f"Average score per game is: {self.score / self.game_counter}")
             print(f"epsilon value is: {self.agent.epsilon}")
@@ -268,7 +304,8 @@ class Game:
             self.snake = Snake(self)
             self.fruit = Fruit()
             self.running = True
-
+            self.points=0
+            self.visited=[]
 pygame.init()
 game = Game()
 game.is_ai = True
